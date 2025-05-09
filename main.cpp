65f73798669c319a0979e7fd1aaa61fc6763c926
SantiagoZorzi <santiagobzor@gmail.com>
2025-05-09 12:44:43 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+string name;
+string nickname;
+name = "Santiago";
+nickname = "Zorzi";
+cout << "Hola "<<name<<" "<<nickname<< endl;
+return 0;
+}
